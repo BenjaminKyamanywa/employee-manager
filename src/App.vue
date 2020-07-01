b2ae3f97cb831854452ac16c0a8a1ec8327710a7
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-01 16:33:56 +0300

    added all REST routes

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,31 @@ export default {
       }
     },
     // PUT method for editing employee
-    
+    async editEmployee(id, updatedEmployee) {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, 
+        {
+          method: 'PUT',
+          body: JSON.stringify(updatedEmployee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' }
+        })
+        const data = await response.json()
+        this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    // DELETE method for deleting employee
+    async deleteEmployee(id) {
+      try {
+          await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: "DELETE"
+          });
+          this.employees = this.employees.filter(employee => employee.id !== id);
+        } catch (error) {
+          console.error(error);
+        }
+    }
   },
 }
 </script>
