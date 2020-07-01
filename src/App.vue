4ffbf2c9f1fcb8bf9a30685f29e53f7dfdf45849
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-01 16:18:23 +0300

    added REST methods for get and post for employees in App.vue

--- src/App.vue
+++ src/App.vue
@@ -24,46 +24,39 @@ export default {
   },
   data() {
     return {
-      employees: [
-        {
-          id: 1,
-          name: 'John Snow',
-          email: 'john@snow.com',
-        },
-        {
-          id: 2,
-          name: 'Daenerys Targaryen',
-          email: 'dany@targaryen.com',
-        },
-        {
-          id: 3,
-          name: 'Robert Baratheon',
-          email: 'robert@baratheon.com',
-        },
-      ],
+      employees: [],
     }
   },
+  mounted() {
+    this.getEmployees()
+  },
   methods: {
-    addEmployee(employee) {
-      const lastId =
-      this.employees.length > 0
-        ? this.employees[this.employees.length - 1].id
-        : 0;
-      const id = lastId + 1;
-      const newEmployee = { ...employee, id};
-
-      this.employees = [...this.employees, newEmployee];
+    // GET method for retrieving employees
+    async getEmployees() {
+      try { 
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        this.employees = data
+      } catch (error) { 
+        console.error(error)
+      }
     },
-    deleteEmployee(id) {
-    this.employees = this.employees.filter(
-      employee => employee.id !== id
-    )
+    // POST method for creating an employee
+    async addEmployee(employee) {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+          method: 'POST',
+          body: JSON.stringify(employee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        })
+        const data = await response.json()
+        this.employees = [...this.employees, data]
+      } catch (error) {
+        console.error(error)
+      }
     },
-    editEmployee(id, updatedEmployee) {
-      this.employees = this.employees.map(employee =>
-        employee.id === id ? updatedEmployee : employee
-      )
-    }
+    // PUT method for editing employee
+    
   },
 }
 </script>
